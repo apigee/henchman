e0ab575fef4ae7e2d47686711e1139cd1a7eae49
Sudharshan Sreenivasan <sudharsh@gmail.com>
2015-09-22 10:43:26 -0700

    Dummy main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/apigee/henchman/henchman"
+)
+
+func main() {
+	tc := make(henchman.TransportConfig)
+	tc["username"] = "vagrant"
+	tc["keyfile"] = "/Users/sudharsh/.vagrant.d/insecure_private_key"
+	tc["hostname"] = "10.224.192.11"
+
+	ssht, _ := henchman.NewSSH(&tc)
+
+	machine := henchman.Machine{}
+	machine.Hostname = "router"
+	machine.Transport = ssht
+
+	task := henchman.Task{}
+	task.Name = "Check it out"
+	task.Module, _ = henchman.NewModule("yum", "package=nginx")
+	task.Run(&machine)
+}
