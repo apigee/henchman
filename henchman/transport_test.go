625d6e3ae21d699dae6cb521475238157edb7d38
Sudharshan Sreenivasan <sudharsh@gmail.com>
2015-09-29 11:21:10 -0700

    Make sure that the test transport returns a valid taskResult json

--- henchman/transport_test.go
+++ henchman/transport_test.go
@@ -2,6 +2,7 @@ package henchman
 
 import (
 	"bytes"
+	"encoding/json"
 	"log"
 )
 
@@ -13,7 +14,19 @@ func (tt *TestTransport) Initialize(config *TransportConfig) error {
 }
 
 func (tt *TestTransport) Exec(cmd string, stdin []byte, sudo bool) (*bytes.Buffer, error) {
-	return bytes.NewBuffer([]byte("cmd " + cmd + " executed")), nil
+	// Create a dummy map mirroring the task result.
+	// We're not using TaskResult directly since changes in that contract
+	// will be caught for free
+	taskResult := map[string]string{
+		"status": "true",
+		"output": "foo",
+		"msg":    "the dude abides",
+	}
+	jsonified, err := json.Marshal(taskResult)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	return bytes.NewBuffer([]byte(jsonified)), nil
 }
 
 func (tt *TestTransport) Put(source, destination string) error {
